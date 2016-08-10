728ae64ab8b9ff3f4fec48bc857037bc934af036
amemic <admir.memic@zalando.de>
2016-08-10 12:23:25 +0200

    Authentication on each request

--- src/main/java/de/admir/config/WebSecurityConfiguration.java
+++ src/main/java/de/admir/config/WebSecurityConfiguration.java
@@ -4,13 +4,12 @@ import de.admir.services.BlabberUserDetailsService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
@@ -37,16 +36,9 @@ class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().fullyAuthenticated().and()
-                .httpBasic().and().
-                csrf().disable();
+        http.httpBasic().and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.headers().frameOptions().disable();
     }
-
-    /*
-    .antMatchers(HttpMethod.POST, "/blabberUsers").hasAnyRole("ADMIN")
-                .antMatchers(HttpMethod.PUT, "/blabberUsers/**").hasAnyRole("ADMIN")
-                .antMatchers(HttpMethod.PATCH, "/blabberUsers/**").hasAnyRole("ADMIN").and()
-     */
 }
--- src/main/java/de/admir/repositories/BlabberUserRepository.java
+++ src/main/java/de/admir/repositories/BlabberUserRepository.java
@@ -4,12 +4,8 @@ import de.admir.models.BlabberUser;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 /**
  * Author: Admir Memic
